f1c4236b2d1e3fef3ead802cbd53544d2e929fd5
LaPlusZ <totti.kasidit@gmail.com>
2026-03-03 13:27:15 +0700

    finalize project

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-//const dns = require('node:dns');
-//dns.setServers(['8.8.8.8', '1.1.1.1']);
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const express = require("express");
 const dotenv = require("dotenv");
